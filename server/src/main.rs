69aba9b1a6b8d2c63216b3cfb36e6bebd508a6bd
Neal Wang <nealwang.sh@protonmail.com>
2024-07-14 16:42:04 -0700

    introduced basic graphql server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use std::{net::SocketAddr, sync::Arc};
+
+use axum::{
+    routing::{get, on, MethodFilter},
+    Extension, Router,
+};
+use juniper::{graphql_object, EmptyMutation, EmptySubscription, RootNode};
+use juniper_axum::{graphql, playground};
+use tokio::net::TcpListener;
+
+#[derive(Clone, Copy, Debug)]
+pub struct Query;
+
+#[graphql_object]
+impl Query {
+    fn hello() -> String {
+        "Hello, World!".to_string()
+    }
+}
+
+type Schema = RootNode<'static, Query, EmptyMutation, EmptySubscription>;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt().init();
+
+    let schema = Schema::new(Query, EmptyMutation::new(), EmptySubscription::new());
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route(
+            "/graphql",
+            on(
+                MethodFilter::GET.or(MethodFilter::POST),
+                graphql::<Arc<Schema>>,
+            ),
+        )
+        .route("/playground", get(playground("/graphql", "/subscriptions")))
+        .layer(Extension(Arc::new(schema)));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let listener = TcpListener::bind(addr).await?;
+
+    tracing::info!("listening on {addr}");
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
